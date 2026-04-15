569432bda210d484866989f008b251f1fa425218
Abu Ameen <113390687+Abu-Ameenudeen@users.noreply.github.com>
2026-04-15 11:41:01 +0530

    Update fetch/callbac to async & await

--- index.js
+++ index.js
@@ -5,35 +5,30 @@ const button = document.getElementById("get-scheme")
 const colors = document.getElementById("colors")
 const hexValues = document.getElementById("hex-values")
 
-button.addEventListener("click", function(){
+button.addEventListener("click", async function(){
 
     const hex = colorPicker.value.substring(1)
     const scheme = mode.value
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${scheme}&count=5`)
-        .then(res => res.json())
-        .then(data => {
+    const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${scheme}&count=5`)
+    const data = await response.json()
+    let colorHTML = ""
+    let hexHTML = ""
 
-            let colorHTML = ""
-            let hexHTML = ""
+    data.colors.forEach(color => {
 
-            data.colors.forEach(color => {
+        colorHTML += `
+            <div class="color-block" 
+            style="background:${color.hex.value}">
+            </div>
+        `
 
-                colorHTML += `
-                    <div class="color-block" 
-                    style="background:${color.hex.value}">
-                    </div>
-                `
+        hexHTML += `
+            <div class="hex">${color.hex.value}</div>
+        `
 
-                hexHTML += `
-                    <div class="hex">${color.hex.value}</div>
-                `
+    })
 
-            })
-
-            colors.innerHTML = colorHTML
-            hexValues.innerHTML = hexHTML
-
-        })
-
+    colors.innerHTML = colorHTML
+    hexValues.innerHTML = hexHTML
 })
